01ebecc3ce104f97974bd83ad63004a51b652493
axeljk <axeljkern@yahoo.com>
2022-08-27 01:49:00 -0400

    Deciding on colors and spacing for site layout.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,8 +5,11 @@ function App() {
 	return (
 		<>
 			<Header />
-			<h1>Hey!</h1>
-			<p>I'm Axel.</p>
+			<main>
+				<h1>Hey!</h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras elementum massa nulla, et ullamcorper augue finibus eu. Curabitur malesuada arcu vel posuere iaculis. Proin ultrices ut nunc ac lobortis. Ut urna quam, rutrum consequat tortor quis, faucibus egestas velit. Donec commodo suscipit dolor non facilisis. Etiam viverra nunc quis sem bibendum, at tempor diam elementum. Suspendisse non ullamcorper ligula. Vivamus nibh ante, iaculis et lobortis in, malesuada in metus. Donec vel condimentum mauris. Vivamus augue arcu, maximus at lacinia vitae, ultricies vitae elit. Proin volutpat dignissim nisl.</p>
+				<p>Aenean lacus justo, venenatis quis semper sed, mollis quis nulla. Nulla ut rutrum diam. Pellentesque molestie vel sem pretium volutpat. Phasellus porta arcu sed bibendum tristique. Ut dui risus, elementum et arcu et, tempus euismod sapien. Vestibulum vel convallis dolor. Duis vel pharetra nisi. Aenean iaculis eros in convallis sagittis. Fusce eu augue sed mauris varius semper ut ut ligula. Duis semper ex et urna lacinia congue. Vestibulum dignissim auctor molestie. Duis sit amet magna eget quam facilisis iaculis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce bibendum scelerisque enim, sed ornare ipsum tristique sit amet. </p>
+			</main>
 			<Footer />
 		</>
 	);
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,11 @@
+import "./style.css";
+
 export default function Footer() {
 	return (
 		<footer>
-			<a href="https://github.com/Axeljk">GitHub</a>
-			<a href="https://www.linkedin.com/in/axel-kern/">LinkedIn</a>
-			<a href="https://twitter.com/ackseljk">Twitter</a>
+			<a href="https://github.com/Axeljk" target="_blank" rel="external noreferrer">GitHub</a>
+			<a href="https://www.linkedin.com/in/axel-kern/" target="_blank" rel="external noreferrer">LinkedIn</a>
+			<a href="https://twitter.com/ackseljk" target="_blank" rel="external noreferrer">Twitter</a>
 		</footer>
 	);
 };
