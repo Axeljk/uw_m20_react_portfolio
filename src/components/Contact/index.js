20e0d5f91effc12894bc61f0ba2b5ef2835e4836
axeljk <axeljkern@yahoo.com>
2022-09-08 00:42:02 -0400

    Populates contact page.

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -5,7 +5,14 @@ export default function Contact() {
 		<main id="Contact">
 			<h2>Contact</h2>
 			<section>
-				<p>Contact fields go here</p>
+				<form>
+					<label for="name">Name:</label><br />
+					<input type="text" id="name" name="name" required /><br />
+					<label for="email">Email:</label><br />
+					<input type="text" id="email" name="email" required /><br />
+					<label for="message">Message:</label><br />
+					<textarea id="message" name="message" required /><br />
+				</form>
 			</section>
 		</main>
 	);
